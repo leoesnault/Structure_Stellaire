d53906534147f0127c889a711b0d7e798ef460f4
leoesnault <esnault.leo@orange.fr>
2016-12-17 20:22:37 +0100

    ajout prog.cpp

---
+++ prog.cpp
@@ -0,0 +1,59 @@
+/*
+Programme de structure stellaire, utilisant les hypothèses :
+-
+-
+-
+-
+-
+*/
+
+
+
+// Librairies utilisées
+#include <iostream> // contient cin, cout ...
+#include <fstream> // permet de lire et écrire dans un fichier
+#include <cmath> // contient des fonctions mathématiques usuelles
+using namespace std;
+// ----
+
+
+
+
+// Définition des fonctions
+
+
+
+
+
+// ----
+
+
+
+
+// Variables relatives aux calculs
+int N=100; // nombre de noeuds de la grille de calcul
+float r; // calcul du rayon
+// ----
+
+
+
+
+// Données physiques
+float R,P,M; // Rayon, pression, masse ...
+float u=(d ln(M))/(d ln(r)),v=-(d ln(P))/(d ln(r)); // variables de Milne
+// ----
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+}
