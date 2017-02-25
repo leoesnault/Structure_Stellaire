d3ac06e691e6b203ad4b1d4e10af048241bcebe0
leoesnault <esnault.leo@orange.fr>
2017-02-25 17:10:06 +0100

    prog OK pour n=1 ! Schéma excentré à gauche, revoir dérivée au centre et calcul q

--- prog.cpp
+++ prog.cpp
@@ -2,7 +2,6 @@
 Programme de calcul de structure stellaire permettant de résoudre l'équation de Lane-Emden
 
 A faire :
--Comment faire pour résoudre un syst non linéaire ?
 -Forcer dérivée à gauche
 -Revoir équation w[N]
 */
@@ -23,10 +22,11 @@ const int 		N=100; // nombre de noeuds de la grille de calcul
 const float		z_max=4.; // valeur maximale de z
 const double 	h=z_max/double(N); // pas en z
 
-const double 	critere_convergence=1.e-8; // critère de convergence entre 2 itérations
+const double 	critere_convergence=1.e-10; // critère de convergence entre 2 itérations
 
 double 			q[N+1],z[N+1],w[N+1]; // Masse, rayon et densité adimensionnées
 double 			w_exact[N+1]; // densitée adimensionnée exacte
+double 			w_prec[N+1]; // Sauvegarde de la valeur précédente de w
 // ----
 
 
@@ -38,43 +38,48 @@ ofstream resultats_temp("resultats_temp.dat");
 
 
 // Définition des fonctions
-void initialisation_w()
+double calcul_zeros(double nombre_de_depart,int i,double tolerance,int nombre_iterations_max)
 {
-	w[0]=1.;
-	for (int i = 1; i <= N; i++) // initialisation w quadratique
-	{
-		w[i]=1.-(1./10.)*pow(z[i],2);
-		/*
-		Tests :
-		1.;
-		1.-(1./2.)*pow(i*h,1);
-		cos(double(i)*h);
-		1.-double(i)*h;
-		sin(z[i])/z[i];
-		*/
-	}
+	// Méthode de Newton-Raphson
+	double second_membre=w_prec[i+1]*((h/z[i])-1.) + w_prec[i-1]*(-(h/z[i])-1.);
+	
+	int steps=0;
+	double X,Y=nombre_de_depart;
+	do	{
+		steps++;
+		// cout << steps << "    " << Y << endl; // affichage du calcul
+		
+		// X_n+1 -> X_n
+		// Y = X_n+1 = X_n  - f(X_n) / Df(X_n)
+		
+		X=Y; 
+		Y=X-(pow(h,2)*pow(X,n)-2.*X-second_membre)/(n*pow(h,2)*pow(X,n-1)-2.);
+		
+		if (steps>nombre_iterations_max) // si le nombre d'itérations maximal est dépassé, afficher un message d'errreur et sortir
+			{
+			cout << "problème de convergence, nombre d'itérations autorisées dépassé"<<endl;
+			break;
+			}
+
+		} while ( abs(Y-X) > tolerance ); // test de tolérance
+
+	return X;
 }
 
 
-// bool test_derivee_centrale_w()
-// {
-// 	if (-3.*w[1]+4.*w[2]-w[3]<critere_convergence)
-// 	{
-// 		return true;
-// 	}
-// 	else
-// 	{
-// 		return false;
-// 	}
-// }
+void initialisation_w()
+{
+	for (int i = 0; i <= N; i++) // initialisation w quadratique
+	{
+		w[i]=1.-(1./10.)*pow(z[i],2);
+	}
+}
 
 
 void calcul_w()
 {
 	w[0]=1.; // Condition au centre de w
-	
-	double w_prec[N+1]; // Sauvegarde de la valeur précédente de w
-	
+		
 	bool convergence=false; // Booléen de convergence pour la boucle while
 	
 	int steps=0; // Nombre de pas de calcul avant convergence. Utile pour afficher tout les X pas
@@ -88,16 +93,18 @@ void calcul_w()
 			w_prec[i]=w[i]; // Sauvegarde de w dans w_prec
 		}
 		
-		w[1]=(1./4.)*(3.*w_prec[0]-w_prec[2]); // Dérivée nulle à gauche. OK???
-
+		w[1]=1.;//(1./4.)*(3.*w_prec[0]-w_prec[2]); // Dérivée nulle à gauche. OK???
+		
 		convergence=true;
 		
-		for (int i = 1; i < N; i++)
+		for (int i = 2; i <= N; i++)
 		{
-			// Calcul de w[i] pour n=1 !!
-			w[i]=(1./(pow(h,2)-2.))*(w_prec[i+1]*((h/z[i])-1.) + w_prec[i-1]*(-(h/z[i])-1.));
+			// Calcul de w[i] pour n=1 !! Avec schéma excentré à gauche
+			// w[i]=(1./(pow(h,2)-2.))*(w_prec[i+1]*((-h/z[i])-1.) + w_prec[i-1]*((h/z[i])-1.));
+			w[i]=(1./(1.+(3.*h/z[i])+pow(h,2)))*(w_prec[i-2]*((-h/z[i])-1.)+w_prec[i-1]*(4.*(h/z[i])+2.));
 			// ---
 			
+			// w[i]=calcul_zeros(w_prec[i],i,critere_convergence,1e3);
 			
 			if (abs(w_prec[i]-w[i])<critere_convergence) // A simplifier ?
 			{
@@ -109,7 +116,7 @@ void calcul_w()
 			}
 		}
 		
-		if (steps%20000==0 or steps==1) // Affichage/Écriture pour le 1er pas puis tout les X pas
+		if (steps%20==0 or steps==1) // Affichage/Écriture pour le 1er pas puis tout les X pas
 		{
 			cout << steps << "	" << w[int(N/2)] << endl; // Affichage du pas et d'une valeur arbitraire de w
 			
@@ -122,7 +129,8 @@ void calcul_w()
 			}
 		}
 		
-		w[N]=(1./(1.+(3.*h/z[N])+pow(h,2)))*(w[N-2]*((-h/z[N])-1.)+w[N-1]*(4.*(h/z[N])+2.));
+		// Pour n=1 :
+		// w[N]=(1./(1.+(3.*h/z[N])+pow(h,2)))*(w_prec[N-2]*((-h/z[N])-1.)+w_prec[N-1]*(4.*(h/z[N])+2.));
 	}
 }
 
@@ -165,13 +173,16 @@ void calcul_z()
 }
 
 
-void calcul_q() // A MODIF
+void calcul_q()
 {
 	q[0]=0.;
-	for (int i = 1; i <= N; i++)
+		
+	for (int i = 1; i < N; i++)
 	{
-		q[i]=1.;
+		q[i]=(-1./z[i])*((-w[i-1]+w[i+1])/2.*h)*pow(z[i],3);
 	}
+	
+	q[N]=(-1./z[N])*((w[N-2]-4.*w[N-1]+3.*w[N])/2.*h)*pow(z[N],3);
 }
 // ----
 
